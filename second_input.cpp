ef5f43cfbb85998bb4fe15e5bde6a33bbc493937
RuiKaiD <rd2464@nyu.edu>
2018-07-05 17:00:26 +0000

    First commit of source.

---
+++ second_input.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	cout << "Please enter your name:\n";
+	int age;
+	string first_name;
+	cin >> first_name;
+	cin >> age;
+	cout << first_name << age;
+}
---
+++ third_input.cpp
@@ -0,0 +1,6 @@
+#include "std_lib_facilities.h"
+int main() {
+	string first_name;
+	string second_name;
+	cin >> first_name >> second_name;
+}
